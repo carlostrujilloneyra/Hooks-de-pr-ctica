4d5226ef56bbae219375408ee936fdbdc72013a6
Carlos Trujillo <augusto21_20@hotmail.com>
2022-03-01 17:59:34 -0500

    actualización

--- src/components/06-useReducer/TodoApp.js
+++ src/components/06-useReducer/TodoApp.js
@@ -1,12 +1,34 @@
+import { useReducer, useState } from 'react';
+import { todoReducer } from './todoReducer';
 import './styles.scss';
 
+// Estado inicial
+const initialState = [{
+	id:new Date().getTime(),
+	desc: 'Aprender React',
+	done: false
+}];
 
 const TodoApp = () => {
-  return (
-	<div>
-		<h1>TodoApp</h1>
-		<hr/>
-	</div>
+	// const [state, dispatch] = useReducer(reducer, initialState, init);
+
+	// Forma más simple
+	const [ todos ] = useReducer(todoReducer, initialState);
+	console.log(todos)
+	return (
+		<div>
+			<h1>TodoApp ({ todos.length })</h1>
+			<hr/>
+
+			<ul>
+				{
+					todos.map(todo => {
+						const { id, desc, done } = todo;
+						return <li key={id}>{desc}</li> ;
+					})
+				}
+			</ul>
+		</div>
   )
 }
 
